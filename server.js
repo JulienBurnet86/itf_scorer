22ae16594f53f82809042246c70cf03915a29bc1
Julien BURNET <julien.burnet@gmail.com>
2022-05-09 10:56:17 +0200

    Fix using sportsradar api

--- server.js
+++ server.js
@@ -1,32 +1,101 @@
 var express = require('express');
 var path = require('path');
+const http = require('http')
+let sockets = [];
+
 var myArgs = process.argv.slice(2);
 if (!myArgs[0])
-	throw new Error("Json file of matches needs to be 1st argment")
-var matchesFile = myArgs[0]
-if (!matchesFile.startsWith("/") && !matchesFile.startsWith(".")) {
-	matchesFile = `./${matchesFile}`
+	throw new Error("Please provide configuration file as first argument")
+var configFile = myArgs[0]
+if (!configFile.startsWith("/") && !configFile.startsWith(".")) {
+	configFile = `./${configFile}`
 }
-var matches = require(matchesFile);
-var currentIdx = 0;
-for (var idx in matches) {
-	const match = matches[idx]
-	if (!match.idx) {
-		match.idx = idx;
-	}
-	if (!match.currentSet) {
-		match.currentSet = 0;
-	}
-	for (var p of match.players) {
-		if (!p.points)
-			p.points = 0;
-		if (!p.games)
-			p.games = [0, 0, 0];
-		if (p.serve === undefined) {
-			p.serve = false;
+var config = require(configFile)
+const keyCounter = []
+for (var key in config.keys) {
+	keyCounter.push({"key": key, "count": 0})
+}
+
+
+//const id = "sr:competition:22540"
+// 22480
+const id = "sr:competition:" + config.competitionId
+
+const currentMatches = {     
+	"Dummy court": {
+		"player1": {
+			"name": "Player1 Name",
+			"games": [6, 4, 4],
+			"point": 15,
+			// optional
+			"serve": false
+		},
+		"player2": {
+			"name": "Player2 Name",
+			"games": [4, 6, 4],
+			"point": 40,
+			// optional
+			"server": true
 		}
 	}
 }
+function update() {
+	// selecting key
+	const apiKey = keyCounter.filter(a => a.count < 1000)[0]
+
+	// 9sa5htyu9ufw8wjyzxws7zbu
+	const url = 'http://api.sportradar.us/tennis/trial/v3/en/schedules/live/summaries.json?api_key=' + apiKey.key
+	console.log("URL is " + url)
+	if (sockets.length > 0) {
+		http.get(url, res => {
+			apiKey.count++
+			console.log("keyCounter", keyCounter)
+			const data = new Array();
+		
+			res.on('data', chunk => {
+				data.push(chunk);
+			});
+			
+			res.on('end', () => {
+				const summaries = JSON.parse(Buffer.concat(data).toString());
+				for (const summary of summaries.summaries) {
+					if (summary.sport_event.sport_event_context.competition.id == id) {
+						const sportEvent = summary.sport_event
+						const eventStatus = summary.sport_event_status
+						const player1 = {
+							name: sportEvent.competitors[0].name,
+							games : [
+								eventStatus.period_scores && eventStatus.period_scores[0] ? eventStatus.period_scores[0].home_score : 0,
+								eventStatus.period_scores && eventStatus.period_scores[1] ? eventStatus.period_scores[1].home_score : 0,
+								eventStatus.period_scores && eventStatus.period_scores[2] ? eventStatus.period_scores[2].home_score : 0
+							],
+							point: eventStatus.game_state ? eventStatus.game_state.home_score : 0
+						}
+						const player2 = {
+							name: sportEvent.competitors[1].name,
+							games : [
+								eventStatus.period_scores && eventStatus.period_scores[0] ? eventStatus.period_scores[0].away_score : 0,
+								eventStatus.period_scores && eventStatus.period_scores[1] ? eventStatus.period_scores[1].away_score : 0,
+								eventStatus.period_scores && eventStatus.period_scores[2] ? eventStatus.period_scores[2].away_score : 0
+							],
+							point: eventStatus.game_state ? eventStatus.game_state.away_score : 0
+						}
+						currentMatches[sportEvent.venue.name] = {
+							"player1" : player1,
+							"player2" : player2
+						}
+					}
+				}
+				console.log(`currentMatches : ${JSON.stringify(currentMatches)}`)
+				sockets.forEach(s => s.send(JSON.stringify(currentMatches)))
+			});
+		})
+	} else {
+		console.log("Nobody is connected :( ")
+	}
+	setTimeout(update, 30000)
+}
+update()
 
 var app = express();
 
@@ -42,18 +111,12 @@ const wsServer = new WebSocket.Server({
   port: 8800
 });
 
-let sockets = [];
 wsServer.on('connection', function(socket) {
+	console.log("Connection, sending : ", currentMatches)
   sockets.push(socket);
-  const initMsg = { 'type': 'init', 'currentIdx': currentIdx, 'matches': matches };
-  socket.send(JSON.stringify(initMsg));
-  // When you receive a message, send that message to every socket.
-  socket.on('message', function(msg) {
-    var match = JSON.parse(msg);
-	matches[match.idx] = match;
-	currentIdx = match.idx;
-    sockets.forEach(s => s.send(msg));
-  });
+  if (currentMatches)
+  socket.send(JSON.stringify(currentMatches));
+  console.log("Message sent")
 
   // When a socket closes, or disconnects, remove it from the array.
   socket.on('close', function() {
